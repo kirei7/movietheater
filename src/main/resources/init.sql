d85e2dc62c11cea25e59854851146279fce220e0
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-04-27 11:49:46 +0300

    Change id type from INT to BIGINT

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,15 +1,16 @@
+/*Aspect tables for jdbcTemplate*/
 DROP TABLE IF EXISTS accessed_price;
 CREATE TABLE accessed_price (
-  id INT PRIMARY KEY,
+  id BIGINT PRIMARY KEY,
   counter BIGINT
 );
 DROP TABLE IF EXISTS accessed_name;
 CREATE TABLE accessed_name (
-  id INT PRIMARY KEY,
+  id BIGINT PRIMARY KEY,
   counter BIGINT
 );
 DROP TABLE IF EXISTS tickets_bought;
 CREATE TABLE tickets_bought (
-  id INT PRIMARY KEY,
+  id BIGINT PRIMARY KEY,
   counter BIGINT
 );
