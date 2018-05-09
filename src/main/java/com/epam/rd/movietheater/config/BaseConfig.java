850626df96d8f47a5e2f7a341450cf952f1211e7
kirei7 <vlad.sereda.y@gmail.com>
2018-05-09 17:15:30 +0300

    Integrate Spring Security with Thymeleaf

--- src/main/java/com/epam/rd/movietheater/config/BaseConfig.java
+++ src/main/java/com/epam/rd/movietheater/config/BaseConfig.java
@@ -1,11 +1,14 @@
 package com.epam.rd.movietheater.config;
 
+import com.epam.rd.movietheater.exception.ManagerInfoFileNotPresentException;
 import com.epam.rd.movietheater.model.entity.Event;
 import com.epam.rd.movietheater.model.entity.User;
+import com.epam.rd.movietheater.security.UserRole;
 import com.epam.rd.movietheater.service.IdentifiableEntityService;
 import com.epam.rd.movietheater.service.auditorium.AuditoriumService;
 import com.epam.rd.movietheater.service.event.EventService;
 import com.epam.rd.movietheater.service.facade.BookingFacade;
+import com.epam.rd.movietheater.service.facade.UserFacade;
 import com.epam.rd.movietheater.service.user.UserService;
 import com.epam.rd.movietheater.util.batchupload.BatchUploader;
 import com.epam.rd.movietheater.util.batchupload.JsonBatchUploader;
@@ -14,13 +17,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
-import java.time.LocalDateTime;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -35,6 +41,8 @@ public class BaseConfig {
     private BookingFacade bookingFacade;
     @Autowired
     private AuditoriumService auditoriumService;
+    @Autowired
+    private UserFacade userFacade;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
@@ -52,7 +60,7 @@ public class BaseConfig {
         return map;
     }
 
-    @EventListener({ContextRefreshedEvent.class})
+    /*@EventListener({ContextRefreshedEvent.class})
     public void init() {
         Event event = new Event("Film one",
                 LocalDateTime.now().plusHours(2),
@@ -62,5 +70,21 @@ public class BaseConfig {
         );
         eventService.save(event);
         bookingFacade.buyTickets(event.getId(), userService.getUserByNickName("user"), new long[]{2, 3, 4, 5});
+    }*/
+
+    @PostConstruct
+    protected void registerManager() {
+        Resource resource = new ClassPathResource("bookingManager.properties");
+        try {
+            Properties props = PropertiesLoaderUtils.loadProperties(resource);
+            User manager = new User(
+                    props.getProperty("manager.nickName"),
+                    props.getProperty("manager.password")
+            );
+            manager.addRole(UserRole.BOOKING_MANAGER);
+            userFacade.registerUser(manager);
+        } catch (IOException ex) {
+            throw new ManagerInfoFileNotPresentException();
+        }
     }
 }
